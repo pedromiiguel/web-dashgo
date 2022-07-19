72c215e2eaf66f8a11eab49eb5ca1347de44b1b1
Pedro Miguel <pedromrap@gmail.com>
2022-07-18 22:07:31 -0300

    feat: integraton header with api

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,25 +1,28 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { authenticatedUser } from '../../store/User/User.selectors';
 
 interface ProfileProps {
   showProfileData?: boolean;
 }
 
 export default function Profile({ showProfileData = true }: ProfileProps) {
+  const user = useSelector(authenticatedUser);
+
   return (
     <Flex align="center">
       {showProfileData && (
         <Box mr="4">
-          <Text>Pedro Miguel</Text>
+          <Text>{user.name}</Text>
           <Text color="gray.300" fontSize="small">
-            pedromrap@gmail.com
+            {user.email}
           </Text>
         </Box>
       )}
       <Avatar
         size="md"
-        name="Pedro Miguel"
-        src="https://github.com/pedromiiguel.png"
+        name={user.name}
       />
     </Flex>
   );
