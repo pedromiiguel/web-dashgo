7ce051bc2c24239a0e7177d6aa20afb1c3b8edd2
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:22:08 -0300

    feat: add parameter options in useUser hook

--- src/services/hooks/useUser.ts
+++ src/services/hooks/useUser.ts
@@ -2,10 +2,8 @@ import { useQuery } from 'react-query';
 import { api } from '../apiClient';
 
 type User = {
-  id: string;
   name: string;
   email: string;
-  createdAt: string;
 };
 
 export async function getUser(id: string): Promise<User> {
@@ -14,8 +12,10 @@ export async function getUser(id: string): Promise<User> {
   return data;
 }
 
-export function useUser(id: string) {
-  return useQuery(['users', id], () => getUser(id), {
+export function useUser(id: string, options: any) {
+  return useQuery(['user', id], () => getUser(id), {
     // staleTime: 1000 * 60 * 10,
+    refetchOnWindowFocus: false,
+    ...options
   });
 }
