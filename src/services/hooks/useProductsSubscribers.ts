c034a2eb42d005b128d3f896a09016b6ed516fe9
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:18:07 -0300

    feat:  Add useProductSubscribers for fetching products subscribers in the week

---
+++ src/services/hooks/useProductsSubscribers.ts
@@ -0,0 +1,54 @@
+import { useQuery } from 'react-query';
+import { api } from '../apiClient';
+import dayjs from 'dayjs';
+
+export interface Count {
+  [key: string]: number;
+}
+
+export async function getProductSubscribers(): Promise<number[]> {
+  const { data } = await api.get('/product/count');
+
+  const weekDays = [...Array(8)].map((_, index) =>
+    dayjs().subtract(index, 'day').toDate().getTime()
+  );
+
+  const subscribersWeek = weekDays.reduce((acc, item) => {
+    const formattedDate = dayjs(item).format('DD/MM/YYYY');
+
+    const existingDate = data?.subscribersWeek.find(
+      (obj: { [key: string]: number }) => {
+        const date = Object.keys(obj)[0];
+
+        return date === formattedDate;
+      }
+    );
+
+    if (existingDate) {
+      const date = Object.keys(existingDate)[0];
+      const count = Object.values(existingDate)[0];
+
+      acc.push({ [String(date)]: Number(count) });
+    } else {
+      acc.push({ [String(formattedDate)]: 0 });
+    }
+
+    return acc;
+  }, [] as Count[]);
+
+  const values = subscribersWeek.map((obj) => {
+    const [value] = Object.values(obj);
+
+    return value;
+  });
+
+  return values;
+}
+
+export function useProductSubscribers(options?: any) {
+  return useQuery(['productSubscribers'], () => getProductSubscribers(), {
+    staleTime: 1000 * 60 * 10,
+    refetchOnWindowFocus: false,
+    ...options
+  });
+}
