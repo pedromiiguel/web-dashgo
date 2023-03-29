241ec2ccd4b1ee3a435917f6865c3c553a5c647b
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:01:18 -0300

    feat: add create users page

--- src/pages/users/create/index.tsx
+++ src/pages/users/create/index.tsx
@@ -6,23 +6,20 @@ import {
   Heading,
   HStack,
   SimpleGrid,
-  VStack,
+  VStack
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-
-import * as yup from 'yup';
-
-import { Input } from '../../components/Form/Input';
-import Header from '../../components/Header';
-import Sidebar from '../../components/Sidebar';
-import { QueryClient, useMutation } from 'react-query';
-import { api } from '../../services/apiClient';
-import { queryClient } from '../../services/queryClient';
 import { useRouter } from 'next/router';
-import { withSSRAuth } from '../../utils/withSSRAuth';
+import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
+import { Input } from '@/components';
+import { withSSRAuth } from '@/utils/withSSRAuth';
+import { queryClient } from '@/services/queryClient';
+import { api } from '@/services/apiClient';
+import * as yup from 'yup';
+import HomeTemplate from '@/templates/Home';
 
 type CreateUserFormData = {
   name: string;
@@ -31,36 +28,36 @@ type CreateUserFormData = {
   password_confirmation: string;
 };
 
-const createUserFormschema = yup
+export const createUserFormschema = yup
   .object({
     name: yup.string().required('Nome obrigatório'),
     email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
-    password: yup.string().required('Senha obrigatória'),
+    password: yup
+      .string()
+      .required('Senha obrigatória')
+      .min(6, 'Digite uma senha com no mínimo 6 caracteres'),
     password_confirmation: yup
       .string()
-      .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais'),
+      .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais')
   })
   .required();
 
 export default function CreateUser() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm({ resolver: yupResolver(createUserFormschema) });
+  const form = useForm<CreateUserFormData>({
+    resolver: yupResolver(createUserFormschema)
+  });
   const router = useRouter();
 
   const createUser = useMutation(
     async (user: CreateUserFormData) => {
-
       const data = {
         email: user.email,
         name: user.name,
-        password: user.password,
+        password: user.password
       };
 
       const response = await api.post('users', {
-        ...data,
+        ...data
       });
 
       return response.data.user;
@@ -68,7 +65,7 @@ export default function CreateUser() {
     {
       onSuccess: () => {
         queryClient.invalidateQueries('users');
-      },
+      }
     }
   );
 
@@ -81,17 +78,15 @@ export default function CreateUser() {
   };
 
   return (
-    <Box>
-      <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar />
+    <HomeTemplate title="Criar novo usuário">
+      <FormProvider {...form}>
         <Box
           as="form"
           flex="1"
           borderRadius="8"
           bg="gray.800"
           p={['6', '8']}
-          onSubmit={handleSubmit(handleCreateUser)}
+          onSubmit={form.handleSubmit(handleCreateUser)}
         >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
@@ -103,19 +98,13 @@ export default function CreateUser() {
               <Input
                 name="name"
                 label="Nome completo"
-                error={errors.name}
-                register={{
-                  ...register('name'),
-                }}
+                error={form.formState.errors.name}
               />
               <Input
                 name="email"
                 type="email"
                 label="E-mail"
-                error={errors.email}
-                register={{
-                  ...register('email'),
-                }}
+                error={form.formState.errors.email}
               />
             </SimpleGrid>
 
@@ -124,19 +113,13 @@ export default function CreateUser() {
                 name="password"
                 label="Senha"
                 type="password"
-                error={errors.password}
-                register={{
-                  ...register('password'),
-                }}
+                error={form.formState.errors.password}
               />
               <Input
                 name="password_confirmation"
                 type="password"
-                error={errors.password_confirmation}
+                error={form.formState.errors.password_confirmation}
                 label="Confirmação de senha"
-                register={{
-                  ...register('password_confirmation'),
-                }}
               />
             </SimpleGrid>
           </VStack>
@@ -146,19 +129,23 @@ export default function CreateUser() {
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button type="submit" isLoading={isSubmitting} colorScheme="pink">
+              <Button
+                type="submit"
+                isLoading={form.formState.isSubmitting}
+                colorScheme="pink"
+              >
                 Salvar
               </Button>
             </HStack>
           </Flex>
         </Box>
-      </Flex>
-    </Box>
+      </FormProvider>
+    </HomeTemplate>
   );
 }
 
-export const getServerSideProps = withSSRAuth(async (ctx) => {
+export const getServerSideProps = withSSRAuth(async () => {
   return {
-    props: {},
+    props: {}
   };
 });
