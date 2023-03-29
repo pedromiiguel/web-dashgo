8a0acf66b0cb8ea84808ba8332c6940c4576ef67
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 18:03:43 -0300

    chore: add index to facilite the import

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import { Input } from './Form/Input';
+import { CurrencyInput } from './Form/CurrencyInput';
+
+import { Header } from './Header/index';
+import { Sidebar } from './Sidebar';
+import { Logo } from './Logo';
+import { Pagination } from './Pagination';
+
+export { Input, CurrencyInput, Header, Sidebar, Logo, Pagination };
