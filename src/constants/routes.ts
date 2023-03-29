475441c2b9c7952d69bd4d16724ced1d08100190
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 16:33:17 -0300

    chore: add routes constant

---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  DASHBOARD: '/',
+  LOGIN: '/sign-in',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+  USERS: '/users',
+  PRODUCTS: '/products',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password'
+};
