766c5e60cbdb758f80b7dba07f13f353814a62ed
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:03:22 -0300

    feat: add edit user page

--- src/pages/users/edit/[id].tsx
+++ src/pages/users/edit/[id].tsx
@@ -7,109 +7,106 @@ import {
   HStack,
   SimpleGrid,
   VStack,
+  Spinner
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import React, { useMemo } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-
-import { Input } from '../../../components/Form/Input';
-import Header from '../../../components/Header';
-import Sidebar from '../../../components/Sidebar';
-import { QueryClient, useMutation } from 'react-query';
-import { api } from '../../../services/apiClient';
-import { queryClient } from '../../../services/queryClient';
+import React from 'react';
 import { useRouter } from 'next/router';
-import { withSSRAuth } from '../../../utils/withSSRAuth';
+import { useMutation } from 'react-query';
+import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
+import { Input } from '@/components';
+import { api } from '@/services/apiClient';
+import { queryClient } from '@/services/queryClient';
+import { withSSRAuth } from '@/utils/withSSRAuth';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { userFormschema } from '../validaton';
-import { useUser } from '../../../services/hooks/useUser';
+import { useUser } from '@/services/hooks/useUser';
+import { ROUTES } from '@/constants/routes';
+import * as yup from 'yup';
+import HomeTemplate from '@/templates/Home';
 
 type EditUserFormData = {
   name: string;
   email: string;
-  password: string;
-  password_confirmation: string;
 };
 
+export const userFormschema = yup
+  .object({
+    name: yup.string().required('Nome obrigatório'),
+    email: yup.string().required('E-mail obrigatório').email('E-mail inválido')
+  })
+  .required();
 export default function EditUser() {
   const router = useRouter();
   const id = router.query.id as string;
 
-  const { data, isLoading } = useUser(id);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm({
-    resolver: yupResolver(userFormschema),
-    defaultValues: { name: data?.name, email: data?.email },
+  const form = useForm<EditUserFormData>({
+    resolver: yupResolver(userFormschema)
   });
 
-  // console.log(data);
-  const createUser = useMutation(
+  const { isLoading } = useUser(id, {
+    onSuccess: (data: EditUserFormData) => {
+      form.reset(data);
+    },
+    enabled: !!id
+  });
+
+  const { mutateAsync } = useMutation(
     async (user: EditUserFormData) => {
-      const data = {
-        email: user.email,
-        name: user.name,
-        password: user.password,
-      };
-
-      const response = await api.post('users', {
-        ...data,
-      });
-
-      return response.data.user;
+      await api.patch(`/user/edit/${id}`, user);
     },
     {
       onSuccess: () => {
         queryClient.invalidateQueries('users');
-      },
+      }
     }
   );
 
-  const handleCreateUser: SubmitHandler<EditUserFormData> = async (values) => {
-    await createUser.mutateAsync(values);
+  const handleUpdateUser: SubmitHandler<EditUserFormData> = async (values) => {
+    try {
+      await mutateAsync(values);
 
-    router.push('/users');
+      router.push(ROUTES.USERS);
+    } catch (error) {
+      form.setError('email', {
+        type: 'custom',
+        message: error.response.data.message
+      });
+    }
   };
 
   return (
-    <Box>
-      <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar />
+    <HomeTemplate title="Editar usuário">
+      <FormProvider {...form}>
         <Box
           as="form"
           flex="1"
           borderRadius="8"
           bg="gray.800"
           p={['6', '8']}
-          onSubmit={handleSubmit(handleCreateUser)}
+          onSubmit={form.handleSubmit(handleUpdateUser)}
         >
           <Heading size="lg" fontWeight="normal">
             Editar usuário
           </Heading>
           <Divider my="6" borderColor="gray.700" />
-          {!isLoading && (
+          {isLoading ? (
+            <Flex align="center" justify="center">
+              <Spinner />
+            </Flex>
+          ) : (
             <VStack spacing="8">
               <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
                 <Input
                   name="name"
                   label="Nome completo"
-                  error={errors.name}
-                  register={{
-                    ...register('name'),
-                  }}
+                  error={form.formState.errors?.name}
                 />
+
                 <Input
                   name="email"
                   type="email"
                   label="E-mail"
-                  error={errors.email}
-                  register={{
-                    ...register('email'),
-                  }}
+                  error={form.formState.errors?.email}
                 />
               </SimpleGrid>
             </VStack>
@@ -120,19 +117,23 @@ export default function EditUser() {
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button type="submit" isLoading={isSubmitting} colorScheme="pink">
+              <Button
+                type="submit"
+                isLoading={form.formState.isSubmitting}
+                colorScheme="pink"
+              >
                 Salvar
               </Button>
             </HStack>
           </Flex>
         </Box>
-      </Flex>
-    </Box>
+      </FormProvider>
+    </HomeTemplate>
   );
 }
 
-export const getServerSideProps = withSSRAuth(async (ctx) => {
+export const getServerSideProps = withSSRAuth(async () => {
   return {
-    props: {},
+    props: {}
   };
 });
