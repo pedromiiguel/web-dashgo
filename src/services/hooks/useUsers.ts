2f0e0cc448bc99e92cf7466d78365a6c7710f7d7
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:37:45 -0300

    feat: add property refetchOnWindowFocus in use user hook

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,11 +1,12 @@
 import { useQuery } from 'react-query';
+import { formatBRDate } from '../../utils/formatBRDate';
 import { api } from '../apiClient';
 
 type User = {
   id: string;
   name: string;
   email: string;
-  createdAt: string;
+  createdAt: Date;
 };
 
 type GetUsersResponse = {
@@ -14,22 +15,18 @@ type GetUsersResponse = {
 };
 
 export async function getUsers(page: number): Promise<GetUsersResponse> {
-  const { data, headers } = await api.get('users', {
-    params: { page, limit: 5 },
+  const { data, headers } = await api.get('user', {
+    params: { page, limit: 5 }
   });
 
   const totalCount = Number(headers['x-total-count']);
 
-  const users = data?.map((user: any) => {
+  const users = data?.users.map((user: User) => {
     return {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      }),
+      createdAt: formatBRDate(user.createdAt)
     };
   });
 
@@ -39,5 +36,6 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 export function useUsers(page: number) {
   return useQuery(['users', page], () => getUsers(page), {
     // staleTime: 1000 * 60 * 10,
+    refetchOnWindowFocus: false
   });
 }
