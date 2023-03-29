63873de39b4d68d2ce695b00ea9905cfe0caeb9b
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 16:40:13 -0300

    chore: import dayjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
+import '../lib/dayjs';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext';
-
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-
 import { queryClient } from '../services/queryClient';
 import { persistedStore, store } from '../store/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
