8248b828123c13831ce566ae69a5ec8fd601b629
Pedro Miguel <pedromrap@gmail.com>
2023-03-29 17:05:03 -0300

    feat: add list users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,7 +5,6 @@ import {
   Flex,
   Heading,
   Icon,
-  Link,
   Spinner,
   Table,
   Tbody,
@@ -14,20 +13,14 @@ import {
   Th,
   Thead,
   Tr,
-  useBreakpointValue,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import NextLink from 'next/link';
-
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-
-import Header from '../../components/Header';
-import Pagination from '../../components/Pagination';
-import Sidebar from '../../components/Sidebar';
-
-import { useUsers } from '../../services/hooks/useUsers';
-import { queryClient } from '../../services/queryClient';
-import { api } from '../../services/apiClient';
+import { Pagination } from '@/components';
+import { useUsers } from '@/services/hooks/useUsers';
+import HomeTemplate from '@/templates/Home';
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -35,123 +28,102 @@ export default function UserList() {
 
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true,
+    lg: true
   });
 
-  async function handlePrefetchUser(userId: string) {
-    await queryClient.prefetchQuery(
-      ['user', userId],
-      async () => {
-        const response = await api.get(`user/${userId}`);
-
-        return response.data;
-      },
-      {
-        staleTime: 1000 * 60 * 10, // 10 minutes
-      }
-    );
-  }
-
   return (
-    <Box>
-      <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar />
-        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">
-              Usuários
-              {!isLoading && isFetching && (
-                <Spinner size="sm" color="gray.500" ml="4" />
-              )}
-            </Heading>
-            <NextLink href="/users/create" passHref>
-              <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              >
-                Criar novo
-              </Button>
-            </NextLink>
+    <HomeTemplate title="Usuários">
+      <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+        <Flex mb="8" justify="space-between" align="center">
+          <Heading size="lg" fontWeight="normal">
+            Usuários
+            {!isLoading && isFetching && (
+              <Spinner size="sm" color="gray.500" ml="4" />
+            )}
+          </Heading>
+          <NextLink href="/users/create" passHref>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </NextLink>
+        </Flex>
+
+        {isLoading ? (
+          <Flex align="center" justify="center">
+            <Spinner />
           </Flex>
+        ) : error ? (
+          <Flex align="center" justify="center">
+            <Text color="red.500">Falha ao obter dados dos usuários</Text>
+          </Flex>
+        ) : (
+          <>
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={['4', '4', '6']} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  {isWideVersion && <Th>Data de cadastro</Th>}
+                  {isWideVersion && <Th w="8"></Th>}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data?.users?.map((user) => {
+                  return (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
 
-          {isLoading ? (
-            <Flex align="center" justify="center">
-              <Spinner />
-            </Flex>
-          ) : error ? (
-            <Flex align="center" justify="center">
-              <Text color="red.500">Falha ao obter dados dos usuários</Text>
-            </Flex>
-          ) : (
-            <>
-              <Table colorScheme="whiteAlpha">
-                <Thead>
-                  <Tr>
-                    <Th px={['4', '4', '6']} color="gray.300" width="8">
-                      <Checkbox colorScheme="pink" />
-                    </Th>
-                    <Th>Usuário</Th>
-                    {isWideVersion && <Th>Data de cadastro</Th>}
-                    {isWideVersion && <Th w="8"></Th>}
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {data?.users?.map((user: any) => {
-                    return (
-                      <Tr key={user.id}>
-                        <Td px={['4', '4', '6']}>
-                          <Checkbox colorScheme="pink" />
-                        </Td>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+
+                      {isWideVersion && (
                         <Td>
-                          <Box>
-                            <Link
-                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                          <NextLink href={`/users/edit/${user.id}`} passHref>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize="16" />
+                              }
                             >
-                              <Text fontWeight="bold">{user.name}</Text>
-                            </Link>
-                            <Text fontSize="sm" color="gray.300">
-                              {user.email}
-                            </Text>
-                          </Box>
+                              Editar
+                            </Button>
+                          </NextLink>
                         </Td>
-                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      )}
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
 
-                        {isWideVersion && (
-                          <Td>
-                            <NextLink href={`/users/edit/${user.id}`} passHref>
-                              <Button
-                                as="a"
-                                size="sm"
-                                fontSize="sm"
-                                colorScheme="purple"
-                                leftIcon={
-                                  <Icon as={RiPencilLine} fontSize="16" />
-                                }
-                              >
-                                Editar
-                              </Button>
-                            </NextLink>
-                          </Td>
-                        )}
-                      </Tr>
-                    );
-                  })}
-                </Tbody>
-              </Table>
-
-              <Pagination
-                totalCountOfRegisters={data?.totalCount}
-                currentPage={page}
-                onPageChange={setPage}
-              />
-            </>
-          )}
-        </Box>
-      </Flex>
-    </Box>
+            <Pagination
+              totalCountOfRegisters={data?.totalCount}
+              currentPage={page}
+              onPageChange={setPage}
+            />
+          </>
+        )}
+      </Box>
+    </HomeTemplate>
   );
 }
